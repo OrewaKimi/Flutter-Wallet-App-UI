61c87ca69b41e5a07c5e18c0fc1b8c5206141680
Kimi Maulana <kimi93.com@gmail.com>
2025-03-20 23:17:55 +0700

    add final, balance, cardNumber, expiryMonth, expiryYear, and color

--- lib/util/my_card.dart
+++ lib/util/my_card.dart
@@ -1,40 +1,58 @@
 import 'package:flutter/material.dart';
 
 class MyCard extends StatelessWidget {
-  const MyCard({Key? key}) : super(key: key);
+  final double balance;
+  final int cardNumber;
+  final int expiryMonth;
+  final int expiryYear;
+  final color;
+  
+  const MyCard({
+    Key? key,
+    required this.balance,
+    required this.cardNumber,
+    required this.expiryMonth,
+    required this.expiryYear,
+    required this.color,
+
+    }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 25),
       child: Container(
-      width: 300,
-      padding: EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        color: Colors.deepPurple[300],
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text('Balance', style: TextStyle(color: Colors.white)),
-          SizedBox(height: 10),
-          Text(
-            '\$5,250.20',
-            style: TextStyle(color: Colors.white, fontSize: 28),
-          ),
-          SizedBox(height: 30),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              // card number
-              Text('**** 3456', style: TextStyle(color: Colors.white)),
-              // card expiration date
-              Text('10/24', style: TextStyle(color: Colors.white)),
-            ],
-          ),
-        ],
-      ),
+        width: 300,
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Balance', style: TextStyle(color: Colors.white)),
+            SizedBox(height: 10),
+            Text(
+              '\$' + balance.toString(),
+              style: TextStyle(color: Colors.white, fontSize: 28),
+            ),
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // card number
+                Text(
+                  cardNumber.toString(), 
+                  style: TextStyle(color: Colors.white)),
+                // card expiration date
+                Text(
+                  expiryMonth.toString() + '/' + expiryYear.toString(),
+                  style: TextStyle(color: Colors.white)),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
