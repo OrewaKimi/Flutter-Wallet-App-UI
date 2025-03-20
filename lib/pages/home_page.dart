0c9ebd6eecd2a80025e6cdce047d8519fcb657db
Kimi Maulana <kimi93.com@gmail.com>
2025-03-20 22:49:39 +0700

    add cards

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -44,7 +44,50 @@ class HomePage extends StatelessWidget {
           ),
           SizedBox(height: 25),
           // cards
-          
+          Container(
+            width: 300,
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.deepPurple[300],
+              borderRadius: BorderRadius.circular(16),
+            ), 
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Balance',
+                  style: TextStyle(
+                    color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 10,
+                  ),
+                Text(
+                  '\$5,250.20',
+                  style: TextStyle(color: Colors.white,
+                  fontSize: 28,
+                    ),
+                  ),
+                  SizedBox(height: 30,
+                  ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    // card number
+                    Text('**** 3456',
+                     style: TextStyle(color: Colors.white,
+                     ),
+                    ),
+                    // card expiration date
+                    Text('10/24',
+                    style: TextStyle(color: Colors.white,
+                     ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
           // 3 buttons -> send + pay + bills
           // column -> stats + transactions
         ],
