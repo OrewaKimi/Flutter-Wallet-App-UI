09246f824774c191bddc381af3a64424bf6bf4f9
Kimi Maulana <kimi93.com@gmail.com>
2025-03-21 05:24:41 +0700

    add shade, blur radius, and spread radius

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -113,13 +113,31 @@ class _HomePageState extends State<HomePage> {
                   Container(
                 height: 100,
                 padding: EdgeInsets.all(20),
-                 decoration: BoxDecoration(color: Colors.white,
+                 decoration: BoxDecoration(color: Colors.green,
                  borderRadius: BorderRadius.circular(20),
+                 boxShadow: [
+                  BoxShadow(color: Colors.grey.shade600,
+                  blurRadius: 40,
+                  spreadRadius: 10,
+                 ),
+                 ],
                  ),
                 child: Center(
                   child: Image.asset('lib/icons/send-money.png'),
                 ),
               ),
+              SizedBox(
+                height: 12,
+              ),
+
+              Text(
+                'Send',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey[700],
+                ),
+                ),
                 ],
               )
             ],
