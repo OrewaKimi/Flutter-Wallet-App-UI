41717126b6d039570e1bc826af9dba0d1bad06ae
Kimi Maulana <kimi93.com@gmail.com>
2025-03-20 22:49:16 +0700

    add cards

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -14,43 +14,41 @@ class HomePage extends StatelessWidget {
           // app bar
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 25.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(children: [
-                Text(
-                'My ',
-                style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(children: [
+                  Text(
+                    'My ',
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-              ),
-              Text(
-                'Cards',
-                style: TextStyle(fontSize: 28),
-              ),
-            ],
-          ),
-
-              // plus button
-              Container(
-                padding: EdgeInsets.all(4),
-                decoration: BoxDecoration(
-                  color: Colors.grey[400],
-                  shape: BoxShape.circle,
+                  Text(
+                    'Cards',
+                    style: TextStyle(fontSize: 28),
+                  ),
+                ]),
+                // plus button
+                Container(
+                  padding: EdgeInsets.all(4),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[400],
+                    shape: BoxShape.circle,
+                  ),
+                  child: Icon(Icons.add),
                 ),
-              
-             child: Icon(Icons.add),
-              ),
-            ],
+              ],
+            ),
           ),
-          )
+          SizedBox(height: 25),
           // cards
-
+          
           // 3 buttons -> send + pay + bills
-
           // column -> stats + transactions
-        ],),
+        ],
+      ),
     );
   }
 }
