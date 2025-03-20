1ecf13f797cc6e47cfd531152e7750a67efa63a6
Kimi Maulana <kimi93.com@gmail.com>
2025-03-20 14:48:17 +0700

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: unused_import
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
