e07610a8e16bcdb456d0a9aefe3b8406be5c427c
Kimi Maulana <kimi93.com@gmail.com>
2025-03-21 05:51:33 +0700

    update Icon image path and button text

--- lib/util/my_button.dart
+++ lib/util/my_button.dart
@@ -1,12 +1,49 @@
 import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget {
-  const MyButton([ Key? key ]) : super(key: key);
+  final String iconImagePath;
+  final String buttonText;
+
+  const MyButton({ 
+    Key? key,
+    required this.iconImagePath,
+    required this.buttonText,
+     }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Column(
+      children: [
+        Container(
+          height: 100,
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Colors.green,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.shade600,
+                blurRadius: 40,
+                spreadRadius: 10,
+              ),
+            ],
+          ),
+          child: Center(
+            child: Image.asset(iconImagePath),
+          ),
+        ),
+        SizedBox(
+          height: 12,
+        ),
+        Text(
+          buttonText, 
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.grey[700],
+          ),
+        ),
+      ],
     );
   }
 }
