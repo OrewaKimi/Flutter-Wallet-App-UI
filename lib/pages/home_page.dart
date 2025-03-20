d822a58e4bbb6f2b90a5d4f7cbf02a4794bf9265
Kimi Maulana <kimi93.com@gmail.com>
2025-03-21 05:47:43 +0700

    add main axis alignment space evenly

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -105,6 +105,7 @@ class _HomePageState extends State<HomePage> {
 
           // 3 buttons -> send + pay + bills
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
 
               // send button
