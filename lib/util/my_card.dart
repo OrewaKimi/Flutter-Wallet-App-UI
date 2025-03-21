818df8f95475eaf4014cbb23e475a7f0af72be25
Kimi Maulana <kimi93.com@gmail.com>
2025-03-21 15:42:20 +0700

    add font weight

--- lib/util/my_card.dart
+++ lib/util/my_card.dart
@@ -30,12 +30,15 @@ class MyCard extends StatelessWidget {
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text('Balance', style: TextStyle(color: Colors.white)),
-            SizedBox(height: 10),
+            Text(
+              'Balance', style: TextStyle(color: Colors.white  )),
             Text(
               '\$' + balance.toString(),
-              style: TextStyle(color: Colors.white, fontSize: 28),
+              style: TextStyle(color: Colors.white, fontSize: 36,
+              fontWeight: FontWeight.bold,
+              ),
             ),
             SizedBox(height: 30),
             Row(
