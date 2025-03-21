215e7028a36240020ea181719eda71c99d91abfc
Kimi Maulana <kimi93.com@gmail.com>
2025-03-21 14:05:00 +0700

    update mainAxisAllignment

--- lib/util/my_list_tile.dart
+++ lib/util/my_list_tile.dart
@@ -14,42 +14,45 @@ class MyListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Row(
-          children: [
-            // icon
-            Container(
-              height: 80,
-              padding: EdgeInsets.all(12),
-              decoration: BoxDecoration(
-                color: Colors.grey[100],
-                borderRadius: BorderRadius.circular(12),
+    return Padding( 
+      padding: const EdgeInsets.only(bottom: 20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              // icon
+              Container(
+                height: 80,
+                padding: EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  color: Colors.grey[100],
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Image.asset('lib/icons/statistics.png'),
               ),
-              child: Image.asset('lib/icons/statistics.png'),
-            ),
-
-            SizedBox(width: 20),
-
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Statistics',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                ),
-                SizedBox(height: 8),
-                Text(
-                  tileSubTitle,
-                  style: TextStyle(fontSize: 16, color: Colors.grey[600]),
-                ),
-              ],
-            ),
-          ],
-        ),
-        Icon(Icons.arrow_forward_ios),
-      ],
+      
+              SizedBox(width: 20),
+      
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Statistics',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    tileSubTitle,
+                    style: TextStyle(fontSize: 16, color: Colors.grey[600]),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Icon(Icons.arrow_forward_ios),
+        ],
+      ),
     );
   }
 }
