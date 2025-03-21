7d43c91971152fce14e333b2a4c237ef79e36a89
Kimi Maulana <kimi93.com@gmail.com>
2025-03-21 13:42:58 +0700

    update statistics

--- lib/util/my_list_tile.dart
+++ lib/util/my_list_tile.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
 
 class MyListTile extends StatelessWidget {
-  const MyListTile({Key? key}): super (key : key);
+  const MyListTile({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: [
+            // icon
+            Container(
+              height: 80,
+              padding: EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                color: Colors.grey[100],
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Image.asset('lib/icons/statistics.png'),
+            ),
 
+            SizedBox(width: 20),
+
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Statistics',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  'Payment and Income',
+                  style: TextStyle(fontSize: 16, color: Colors.grey[600]),
+                ),
+              ],
+            ),
+          ],
+        ),
+        Icon(Icons.arrow_forward_ios),
+      ],
     );
   }
 }
